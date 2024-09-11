a8ffaf452eea6807d1d096da761733ad7f379a94
Husepas <86587439+Husepas@users.noreply.github.com>
2024-09-11 21:53:19 +0530

    Update Exp6Q3.cpp

--- Exp6Q3.cpp
+++ Exp6Q3.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main()
 {
-    int n,a[10];
+    int n,a[10],b[10];
     cout<<"Enter n: ";
     cin>>n;
     cout<<"Enter elements of array 1 ";
@@ -11,13 +11,13 @@ int main()
         cin>>a[i];
     cout<<"Enter elements of array 2 ";
     for (int j=0; j<n; j++)
-        cin>>a[j];
+        cin>>b[j];
     cout<<"Intersections are :-\n";
     for (int i=0; i<n; i++)
     {
         for (int j=0; j<n; j++)
         {
-            if (a[i]==a[j])
+            if (a[i]==b[j])
                 cout<<a[i]<<" ";
         }
     }
